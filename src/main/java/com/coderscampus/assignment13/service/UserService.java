f4bddafacc559e6e51ab50960a9a7ea41ba1fe1a
letziegh <148257470+letziegh@users.noreply.github.com>
2024-09-06 20:53:40 -0400

    Final commit to A13

--- src/main/java/com/coderscampus/assignment13/service/UserService.java
+++ src/main/java/com/coderscampus/assignment13/service/UserService.java
@@ -14,174 +14,115 @@ import com.coderscampus.assignment13.domain.Account;
 import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AccountRepository;
 import com.coderscampus.assignment13.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
-	@Autowired
-	private UserRepository userRepo;
-	@Autowired
-	private AccountRepository accountRepo;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private AccountRepository accountRepo;
 
-	@Autowired
-	private AddressRepository addressRepo;
+    @Autowired
+    private AddressRepository addressRepo;
 
-	public List<User> findByUsername(String username) {
-		return userRepo.findByUsername(username);
-	}
+    public List<User> findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
 
-	public List<User> findByNameAndUsername(String name, String username) {
-		return userRepo.findByNameAndUsername(name, username);
-	}
+    public List<User> findByNameAndUsername(String name, String username) {
+        return userRepo.findByNameAndUsername(name, username);
+    }
 
-	public List<User> findByCreatedDateBetween(LocalDate date1, LocalDate date2) {
-		return userRepo.findByCreatedDateBetween(date1, date2);
-	}
+    public List<User> findByCreatedDateBetween(LocalDate date1, LocalDate date2) {
+        return userRepo.findByCreatedDateBetween(date1, date2);
+    }
 
-	public User findExactlyOneUserByUsername(String username) {
-		List<User> users = userRepo.findExactlyOneUserByUsername(username);
-		if (users.size() > 0)
-			return users.get(0);
-		else
-			return new User();
-	}
+    public User findExactlyOneUserByUsername(String username) {
+        List<User> users = userRepo.findExactlyOneUserByUsername(username);
+        if (users.size() > 0)
+            return users.get(0);
+        else
+            return new User();
+    }
 
-	public Set<User> findAll() {
-		return userRepo.findAllUsersWithAccountsAndAddresses();
-	}
+    public Set<User> findAll() {
+        return userRepo.findAllUsersWithAccountsAndAddresses();
+    }
 
-	public User findById(Long userId) {
-		Optional<User> userOpt = userRepo.findById(userId);
-		return userOpt.orElse(new User());
-	}
+    public User findById(Long userId) {
+        Optional<User> userOpt = userRepo.findById(userId);
+        return userOpt.orElse(new User());
+    }
 
-	public User saveUser(User user) {
-		if (user.getUserId() == null) {
-			Account checking = new Account();
-			checking.setAccountName("Checking Account");
-			checking.getUsers().add(user);
-			Account savings = new Account();
-			savings.setAccountName("Savings Account");
-			savings.getUsers().add(user);
+    public User saveUser(User user) {
+        if (user.getUserId() == null) {
+            Account checking = new Account();
+            checking.setAccountName("Checking Account");
+            checking.getUsers().add(user);
+            Account savings = new Account();
+            savings.setAccountName("Savings Account");
+            savings.getUsers().add(user);
 
-			user.getAccounts().add(checking);
-			user.getAccounts().add(savings);
-			accountRepo.save(checking);
-			accountRepo.save(savings);
-			user = userRepo.save(user);
-			//return userRepo.save(user);
-		}else {
-			// Updating an existing user, ensure address handling is correct
-			if (user.getAddress() != null) {
-				Address address = user.getAddress();
-				address.setUser(user); // Set the reference back to the user
-				addressRepo.save(address); // Save or update the address
-			}
-		}
+            user.getAccounts().add(checking);
+            user.getAccounts().add(savings);
+            accountRepo.save(checking);
+            accountRepo.save(savings);
+            user = userRepo.save(user);
 
-		return userRepo.save(user); // Save or update the user
-	}
+        } else {
 
+            if (user.getAddress() != null) {
+                Address address = user.getAddress();
+                address.setUser(user); // Set the reference back to the user
+                addressRepo.save(address); // Save or update the address
+            }
+        }
 
-//		if (user.getAddress() != null) {
-//			Address address = user.getAddress();
-//			address.setUser(user); // Set the reference back to the user
-//			address.setUserId(user.getUserId()); // Make sure the IDs match
-//
-//			// Save the address after the user ID is generated
-//			addressRepo.save(address);
-//
-//		}
-
-//		return userRepo.save(user);
-//	}
-
-		// Check if the address is new (transient) and save it before saving the user
-//		if (user.getAddress() != null && user.getAddress().getUserId() == null) {
-//			Address address = user.getAddress();
-//			// Set the reference back to the user
-//			address.setUser(user);
-//			// Save the address first
-//			address = addressRepo.save(address);
-//			user.setAddress(address);
-//		}
-
-		public User updateUser (User user){
-			User existingUser = userRepo.findById(user.getUserId())
-					.orElseThrow(() -> new RuntimeException("User not found"));
-
-			// Update fields
-			existingUser.setUsername(user.getUsername());
-			existingUser.setPassword(user.getPassword());
-			existingUser.setName(user.getName());
+        return userRepo.save(user); // Save or update the user
+    }
 
 
-			if (existingUser.getAddress() != null) {
-				Address address = existingUser.getAddress();
-				if (address == null) {
-					// If the user doesn't have an address, create one
-					address = new Address();
-					address.setUser(existingUser);
-				}
+    @Transactional
+    public User updateUser(User user) {
+        User existingUser = userRepo.findById(user.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        existingUser.setUsername(user.getUsername());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setName(user.getName());
+
+        Address address = existingUser.getAddress();
+        if (address == null) {
+
+            address = new Address();
+            address.setUser(existingUser);
+        }
 
 
-				address = existingUser.getAddress();
-				address.setAddressLine1(user.getAddress().getAddressLine1());
-				address.setAddressLine2(user.getAddress().getAddressLine2());
-				address.setCity(user.getAddress().getCity());
-				address.setRegion(user.getAddress().getRegion());
-				address.setCountry(user.getAddress().getCountry());
-				address.setZipCode(user.getAddress().getZipCode());
-//			} else {
-//				address = user.getAddress();
-//				address.setUser(existingUser);
-//				address.setUserId(existingUser.getUserId());
-//			}
+        address.setAddressLine1(user.getAddress().getAddressLine1());
+        address.setCity(user.getAddress().getCity());
 
-				// Save the address
-				addressRepo.save(address);
-				existingUser.setAddress(address);
-			}
-			return userRepo.save(existingUser);
-		}
-			// Update the address
-//			if (user.getAddress() != null) {
-//				Address address = user.getAddress();
-//				address.setUser(existingUser);
-//				addressRepo.save(address); // Save the address
-//				existingUser.setAddress(address);
-//			}
-//			Address address;
-//			if (existingUser.getAddress() != null) {
-//				address = existingUser.getAddress();
-//				address.setAddressLine1(user.getAddress().getAddressLine1());
-//				address.setCity(user.getAddress().getCity());
-//				address.setRegion(user.getAddress().getRegion());
-//				address.setCountry(user.getAddress().getCountry());
-//				address.setZipCode(user.getAddress().getZipCode());
-//			} else {
-//				address = user.getAddress();
-//				address.setUser(existingUser);
-//			}
-//			// Save the updated/new address
-//			addressRepo.save(address);
-//			existingUser.setAddress(address);
-//
-//			return userRepo.save(existingUser);
-//		}
+        addressRepo.save(address);
+
+        existingUser.setAddress(address);  // Link the updated address to the user
+
+        return userRepo.save(existingUser);
+    }
 
 
-	public Address findAddressByUserId(Long userId) {
-		Optional<User> userOpt = userRepo.findById(userId);
-		if (userOpt.isPresent()) {
-			return userOpt.get().getAddress();
-		} else {
-			return null;
-		}
-	}
+    public Address findAddressByUserId(Long userId) {
+        Optional<User> userOpt = userRepo.findById(userId);
+        if (userOpt.isPresent()) {
+            return userOpt.get().getAddress();
+        } else {
+            return null;
+        }
+    }
 
-	public void delete(Long userId) {
-		userRepo.deleteById(userId);
-	}
+    public void delete(Long userId) {
+        userRepo.deleteById(userId);
+    }
 }
 
--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -19,104 +19,86 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-	
-	@Autowired
-	private UserService userService;
 
-	@Autowired
-	private AccountService accountService;
-	
-	@GetMapping("/register")
-	public String getCreateUser (ModelMap model) {
-		
-		model.put("user", new User());
-		
-		return "register";
-	}
-	
-	@PostMapping("/register")
-	public String postCreateUser (User user) {
-		System.out.println(user);
-		userService.saveUser(user);
-		return "redirect:/register";
-	}
-	
-	@GetMapping("/users")
-	public String getAllUsers (ModelMap model) {
-		Set<User> users = userService.findAll();
-		
-		model.put("users", users);
-		if (users.size() == 1) {
-			model.put("user", users.iterator().next());
-		}
-		
-		return "users";
-	}
-	
-	@GetMapping("/users/{userId}")
-	public String getOneUser (ModelMap model, @PathVariable Long userId) {
-		User user = userService.findById(userId);
-		model.put("users", Arrays.asList(user));
-		model.put("user", user);
-		return "users";
-	}
-	
-	@PostMapping("/users/{userId}")
-	public String postOneUser (User user) {
-		//userService.saveUser(user);
-		userService.updateUser(user);
-		System.out.println(user.getUsername());
-		System.out.println(user.getPassword());
-		return "redirect:/users/"+user.getUserId();
-	}
-	
-	@PostMapping("/users/{userId}/delete")
-	public String deleteOneUser (@PathVariable Long userId) {
-		userService.delete(userId);
-		return "redirect:/users";
-	}
-	@GetMapping("/users/{userId}/accounts/new")
-	public String showNewAccountForm(@PathVariable Long userId, Model model) {
-		model.addAttribute("userId", userId); // Pass the userId to the form
-		model.addAttribute("account", null); // New account object for the form
-		return "account-form"; // Account creation form template (Thymeleaf view)
-	}
-	@PostMapping("/users/{userId}/accounts/new")
-	public String createNewAccount(@PathVariable Long userId, @RequestParam("name") String accountName) {
-		accountService.createNewAccount(userId, accountName);
-		return "redirect:/users/" + userId; // Redirect back to the user page after creation
-	}
-//	@PostMapping("/users/{userId}/accounts/new")
-//	public String createNewAccount(@PathVariable Long userId, @RequestParam(value = "name", required = false) String name, Model model) {
-//		if (name == null || name.isEmpty()) {
-//			// No name provided, so show the account creation page
-//			model.addAttribute("userId", userId);
-//			model.addAttribute("account", null); // Ensure it's null for the creation page
-//			return "account-form"; // Return the account form template
-//		}
+    @Autowired
+    private UserService userService;
 
-		// If the name is provided, create the account
-//		accountService.createNewAccount(userId, name);
-//		return "redirect:/users/" + userId;
-//	}
+    @Autowired
+    private AccountService accountService;
 
-	//	@PostMapping("/users/{userId}/accounts/new")
-//	public String createNewAccount(@PathVariable Long userId, @RequestParam (value = "name", required = false) String name ) {
-//		accountService.createNewAccount(userId, name);
-//		//model.addAttribute("user", userService.findById(userId));
-//		return "redirect:/users/" + userId;
-//	}
-	@GetMapping("/users/{userId}/accounts/{accountId}")
-	public String getAccountDetails(@PathVariable Long userId, @PathVariable Long accountId, Model model) {
-		Account account = accountService.findAccountByAccountId(accountId);
-		model.addAttribute("account", account);
-		return "account-form";
-	}
+    @GetMapping("/register")
+    public String getCreateUser(ModelMap model) {
 
-	@PostMapping("/users/{userId}/accounts/{accountId}")
-	public String updateAccountName(@PathVariable Long userId, @PathVariable Long accountId, @RequestParam String name) {
-		accountService.updateAccountName(accountId, name);
-		return "redirect:/users/" + userId ;
-	}
+        model.put("user", new User());
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postCreateUser(User user) {
+        System.out.println(user);
+        userService.saveUser(user);
+        return "redirect:/register";
+    }
+
+    @GetMapping("/users")
+    public String getAllUsers(ModelMap model) {
+        Set<User> users = userService.findAll();
+
+        model.put("users", users);
+        if (users.size() == 1) {
+            model.put("user", users.iterator().next());
+        }
+
+        return "users";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getOneUser(ModelMap model, @PathVariable Long userId) {
+        User user = userService.findById(userId);
+        model.put("users", Arrays.asList(user));
+        model.put("user", user);
+        return "users";
+    }
+
+    @PostMapping("/users/{userId}")
+    public String postOneUser(@PathVariable Long userId, User user, Model model) {
+        User updatedUser = userService.updateUser(user);
+        model.addAttribute("user", updatedUser);
+        return "redirect:/users/" + userId;
+    }
+
+    @PostMapping("/users/{userId}/delete")
+    public String deleteOneUser(@PathVariable Long userId) {
+        userService.delete(userId);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/{userId}/accounts/new")
+    public String showNewAccountForm(@PathVariable Long userId, Model model) {
+        model.addAttribute("userId", userId);
+        model.addAttribute("account", null);
+        return "account-form";
+    }
+
+    @PostMapping("/users/{userId}/accounts/new")
+    public String createNewAccount(@PathVariable Long userId, @RequestParam("name") String accountName) {
+        accountService.createNewAccount(userId, accountName);
+        return "redirect:/users/" + userId;
+    }
+
+
+    @GetMapping("/users/{userId}/accounts/{accountId}")
+    public String getAccountDetails(@PathVariable Long userId, @PathVariable Long accountId, Model model) {
+        Account account = accountService.findAccountByAccountId(accountId);
+        model.addAttribute("account", account);
+        return "account-form";
+    }
+
+    @PostMapping("/users/{userId}/accounts/{accountId}")
+    public String updateAccountName(@PathVariable Long userId, @PathVariable Long accountId, @RequestParam String name) {
+        accountService.updateAccountName(accountId, name);
+        return "redirect:/users/" + userId;
+    }
 
 }
